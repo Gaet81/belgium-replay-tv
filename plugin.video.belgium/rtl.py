2ae5665425e50ec3600b6a44571e9dfe5cdf2af3
Gaet81 <matthysgaetan@hotmail.com>
2017-02-22 19:30:07 +0000

    Updating RTL
    RTL channels (rtltvi, club and plug) are now working fine

--- plugin.video.belgium/rtl.py
+++ plugin.video.belgium/rtl.py
@@ -1,97 +1,134 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
 import re
-
 import channel
+import HTMLParser
+from bs4 import BeautifulSoup
 
-channels = {'rtltvi': 'http://www.rtl.be/rtltvi',
-            'clubrtl': 'http://www.clubrtl.be',
-            'plugrtl': 'http://www.plugrtl.be'}
+channels = {'rtltvi': 'http://www.rtl.be/tv/rtltvi',
+            'clubrtl': 'http://www.rtl.be/tv/clubrtl',
+            'plugrtl': 'http://www.rtl.be/tv/plugrtl'}
 
-id2skip = [str(x) for x in [2601,4497,3098,4452,4509,1585,1103,4388,657,767,4296,4468,701]]
-only_good_cat = {'2616': 'Le journal'}
+#id2skip = [str(x) for x in [2601,4497,3098,4452,4509,1585,1103,4388,657,767,4296,4468,701]]
+#only_good_cat = {'2616': 'Le journal'}
 
 class Channel(channel.Channel):
     def get_main_url(self):
-        return 'http://www.rtl.be/' 
+        return 'http://www.rtl.be/tv/'
 
-    def get_categories(self, skip_empty_id = True):
-        data = channel.get_url(self.main_url + self.channel_id + '/page/toutes-les-videos/237.aspx')
-        regex = r"""(?i)""" + self.channel_id + r"""(/categorie/[^/]+/(\d+)\.aspx)[^>]+><img[^"]+"([^"]+)[^>]*></a>\s*<h3>([^<]+)"""
-        print regex
-        channels = []
-        channels_data = {}
-        for url, id, img, name in re.findall(regex, data):
-            if skip_empty_id and id in id2skip:
-                continue
-            if id not in only_good_cat or name.find(only_good_cat[id]) != -1:
-                channels_data[url] = (name, img, id)
-        channels = channels_data.keys()
-        channels.sort(lambda x, y: cmp(str.lower(channels_data[x][0]), str.lower(channels_data[y][0])))
-        for url in channels:
-            name, img, id = channels_data[url]
-            name = channel.htmlentitydecode(name)
-            channel.addDir(name, img, channel_id=self.channel_id, url=url, action='show_videos', id=id)
+    def get_categories(self):
+        channel_url = self.main_url + self.channel_id
+        channel.addDir("direct", 'DefaultVideo.png', channel_id=self.channel_id, url=channel_url+'/live', action='show_videos', id='direct', direct='1')
+        data_url = channel_url + '/emissions'
+        print ('emissions' + data_url)
+        data = channel.get_url(data_url)
+        soup = BeautifulSoup(data, 'html.parser')
+        main = soup.main
+        section = soup.find('section', type='glossary')
+        letters = section.find_all('div', {'class':'letter'})
+        for letter in letters:
+            name = letter.string
+            channel.addDir(name, 'DefaultVideo.png', channel_id=self.channel_id, url=letter.parent.prettify().encode('UTF-8'), action='show_subcategories', id=name)
 
+    def get_subcategories(self,datas):
+        data = datas.get('url')
+        print ('letter data' + data)
+        letter = BeautifulSoup(data, 'html.parser')
+        links = letter.find_all('a')
+        for link in links:
+            name = link.string
+            cat_id = link['href']
+            cat_url = 'http:'+cat_id
+            #print ('1 emission ' + cat_url)
+            dataCat = channel.get_url(cat_url) #to add only emissions with video 
+            regex = r"""<section type="videos"(.)"""
+            if re.search(regex,dataCat):
+                regex1 = r"""<meta property="og:image" content="(.+)\""""
+                img = 'http:'+re.findall(regex1,dataCat)[0]
+                url = cat_url + '/videos/playlist_' + cat_url.rsplit('/', 1)[-1]
+                #print ('playlist emissions '+url)
+                channel.addDir(name, img, channel_id=self.channel_id, url=url, action='show_videos', id=cat_id)
+##            cat_url = 'http:'+cat_id
+##            #print ('1 emission ' + cat_url)
+##            #add only emissions with video
+##            url = cat_url + '/videos/playlist_' + cat_url.rsplit('/', 1)[-1]
+##            if channel.get_status(url):
+##                img = 'DefaultVideo.png'
+##                channel.addDir(name, img, channel_id=self.channel_id, url=url, action='show_videos', id=cat_id)
+# 
     def get_videos(self, datas):
         url = datas.get('url')
         if datas.get('direct', False):
             self.get_direct_videos()
             return
-        data = channel.get_url(self.main_url + self.channel_id + url)
-        regex = r"""<img src="([^"]+)"[^>]*><A class=[^']+'(\d+)[^>]+>[a-z0-9A-Z\s]+<BR>([^<]+)"""
-        ids = []
-        for img, id, title in re.findall(regex, data):
-            if id in ids:
-                continue
-            ids.append(id)
+        data = channel.get_url(url)
+        soup = BeautifulSoup(data, 'html.parser')
+        main = soup.main
+        section = soup.find('section', type='videos-list')
+        articles = section.find_all('article', card='video')
+        for article in articles:
+            a = article.find('a')
+            title = article.h3.string
+            prog_id = a['href']
+            prog_url = 'http:'+prog_id
+            video = channel.get_url(prog_url)
+            regex = r"""videoid="(.+)" """
+            id = re.findall(regex,video)[0]
+            img = 'http:'+ article.find('img',{'src':True})['src']
             vurl = channel.array2url(channel_id=self.channel_id, url=id, action='play_video')
-            channel.addLink(channel.htmlentitydecode(title), vurl, img)
-        regex = r"""<A href="/""" + self.channel_id + """/video/(\d+)\.aspx[^"]*"[^>]*><img src="([^"]+?)"[^>]*></A>\s*?<H3>([^<]+)</H3>"""
-        for id, img, title in re.findall(regex, data):
-            if id in ids:
-                continue
-            ids.append(id)
-            vurl = channel.array2url(channel_id=self.channel_id, url=id, action='play_video')
-            channel.addLink(channel.htmlentitydecode(title), vurl, img)
+            channel.addLink(title, vurl, img)
 
     def get_direct_videos(self):
-        url = 'http://www.rtl.be/rtltvi/page/les-directs-rtl-tvi/258.aspx'
+        url = self.main_url + self.channel_id + '/live'
         data = channel.get_url(url)
-        regex = r"""OtherLiveItem(Big|)Img ><A href="http://www.rtl.be/[^/]+/[^/]+/[^/]+/(\d+)\.aspx"><img src="([^"]+)"[^>]+></A></DIV>\s+<[^>]+>([^<]+)</DIV>\s+<[^>]+>([^<]+)"""
-        for big, id, img, title, time in re.findall(regex, data):
-            title = channel.htmlentitydecode(title + ' - ' + time)
+        soup = BeautifulSoup(data, 'html.parser')
+        main = soup.main
+        section = soup.find('section', {'video':True})
+        if not section:
+            pass
+        else:
+            time = section.find('time')
+            if not time:
+                title = section.find('h3').string
+            else:
+                title = section.find('h3').string + ' - ' + time.string
+            id = section.find('script', {'liveid':True})['liveid']
+            img =''
             vurl = channel.array2url(channel_id=self.channel_id, url=id, action='play_video', direct='1')
             channel.addLink(title, vurl, img)
 
     def play_video(self, datas):
         id = datas.get('url')
+        url = 'http://www.rtl.be/videos/player/replays/' + id[:-3] + '000/' + id + '.xml'
         if datas.get('direct'):
-            id = self.get_direct_video_id(id)
-        if channel.in_xbmc:
-            from elementtree import ElementTree
-        else:
-            from xml.etree import ElementTree
-        data = channel.get_url(self.main_url + self.channel_id + '/GetFlashParams.aspx?id=%s&bEmbed=0&sDummyPath=' % id)
-        tree = ElementTree.fromstring(data)
-        vpo = tree.find('VPO')
-        title, img, url = vpo.find('Title').text, vpo.find('Thumbnail').text, vpo.find('URL').text
-        channel.playUrl(url)
-    
-    def get_direct_video_id(self, id):
-        url = 'http://www.rtl.be/rtltvi/live//' + id + '.aspx'
+            url = 'http://www.rtl.be/videos/player/lives/' + id[:-3] + '000/'+ id + '.xml'   
         data = channel.get_url(url)
-        regex = r"""VideoID=(\d+)"""
-        id = re.findall(regex, data)[0]
-        return id
+        #print('xml '+ url)
+        soup = BeautifulSoup(data, 'html.parser')
+        title = soup.find('title').string
+        img = 'http:' + soup.find('thumbnail').string
+        urlTag = ['url_hls','url_mp4','url_hds','url_flash']
+        i = 0
+        flow_url = ''
+        while flow_url == '' and i < len(urlTag):
+            flow_url = soup.find(urlTag[i]).string
+            i += 1
+        channel.playUrl(flow_url)
+    
+##    def get_direct_video_id(self, id):
+##        url = 'http://www.rtl.be/videos/player/lives/' + id[:-3] + '000/'+ id + '.xml'
+##        data = channel.get_url(url)
+##        regex = r"""<URL_HLS>(.+)</"""
+##        id = re.findall(regex, data)[0]
+##        return id
 
 
 if __name__ == "__main__":
     import sys
     args = sys.argv
     if len(args) == 1:
-        print 'rtltvi clubrtl plugrtl'
+        print ('rtltvi clubrtl plugrtl')
     elif len(args) == 3:
         if args[2] == 'direct':
             Channel({'channel_id': args[1], 'action': 'show_videos', 'direct': 1})
